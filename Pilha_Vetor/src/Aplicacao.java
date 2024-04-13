0c639a097817906fffe3bfbedd7a0b5795f813ce
evelinealonso <69216337+evelinealonso@users.noreply.github.com>
2024-04-13 15:14:04 -0300

    Criação da carta

--- Pilha_Vetor/src/Aplicacao.java
+++ Pilha_Vetor/src/Aplicacao.java
@@ -2,5 +2,13 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+		try {
+			Carta carta = new Carta("COPAS", "AS");
+		
+			System.out.println(carta.getNaipe());
+			System.out.println(carta.getValor());
+		} catch (Exception excecao) {
+			System.out.println(excecao.getMessage());
+		}
 	}
 }
