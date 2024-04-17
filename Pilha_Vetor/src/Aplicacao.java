9405ac8f465fedf6581cb4ffc305c64593594279
Luca Azalim <lucaazalim@gmail.com>
2024-04-17 15:22:41 -0300

    Removendo a captura de exceções na Aplicacao.
    Dadas as mudanças dos commits anteriores, deixa de ser necessário capturar exceções na classe Aplicacao.

--- Pilha_Vetor/src/Aplicacao.java
+++ Pilha_Vetor/src/Aplicacao.java
@@ -1,29 +1,26 @@
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		
+
 		Pilha<Carta> minhaPilha = new Pilha<Carta>(5);
 		Carta item, desempilhado;
-		
-		try {
-			item = new Carta("COPAS", "AS");
-			minhaPilha.empilhar(item);
-		
-			item = new Carta("ESPADA", "DEZ");
-			minhaPilha.empilhar(item);
-		
-			item = new Carta("OUROS", "NOVE");
-			minhaPilha.empilhar(item);
-		
-			desempilhado = minhaPilha.desempilhar();
-			System.out.println("Item desempilhado: " + desempilhado.getNaipe() 
-			+ ", " + desempilhado.getValor());  // OUROS, NOVE
-		
-			item = minhaPilha.consultarTopo();
-			System.out.println("Item do topo: " + item.getNaipe() + ", "
-					+ item.getValor());  // ESPADA, DEZ
-		} catch (Exception excecao) {
-			System.out.println(excecao.getMessage());
-		}
+
+		item = new Carta("COPAS", "AS");
+		minhaPilha.empilhar(item);
+
+		item = new Carta("ESPADA", "DEZ");
+		minhaPilha.empilhar(item);
+
+		item = new Carta("OUROS", "NOVE");
+		minhaPilha.empilhar(item);
+
+		desempilhado = minhaPilha.desempilhar();
+		System.out.println("Item desempilhado: " + desempilhado.getNaipe()
+				+ ", " + desempilhado.getValor()); // OUROS, NOVE
+
+		item = minhaPilha.consultarTopo();
+		System.out.println("Item do topo: " + item.getNaipe() + ", "
+				+ item.getValor()); // ESPADA, DEZ
+
 	}
 }
