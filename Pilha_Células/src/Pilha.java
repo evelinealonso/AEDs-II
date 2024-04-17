ba97543ad130f4db32b9981eb36e29301f68df18
Luca Azalim <lucaazalim@gmail.com>
2024-04-17 15:27:15 -0300

    Aplicando boas práticas à classe Pilha.
    - Aplicando a técnica de "early return" (ou retorno rápido) para simplificar a legibilidade;
    - Removendo o uso de elses desnecessários, o que também simplifica a legibilidade;
    - Invertendo condições que usavam o operador lógico NOT, o que também simplifica a legibilidade.
    - Substituindo o uso de Exceptions por RuntimeExceptions. O uso de RuntimeExceptions (unchecked exceptions) específicas é mais adequado do que o uso de Exceptions (checked exceptions). A classe Deque, que corresponde à implementação de pilhas no Java, utiliza RuntimeExceptions.

--- Pilha_Células/src/Pilha.java
+++ Pilha_Células/src/Pilha.java
@@ -1,52 +1,52 @@
+import java.util.NoSuchElementException;
+
 public class Pilha<T> {
 
 	private Celula<T> topo;
 	private Celula<T> fundo;
-	
+
 	public Pilha() {
-		
+
 		Celula<T> sentinela;
-		
+
 		sentinela = new Celula<T>();
 		fundo = sentinela;
 		topo = sentinela;
 	}
-	
+
 	public boolean pilhaVazia() {
-		
-		if (fundo == topo)
-			return true;
-		else
-			return false;
+		return fundo == topo;
 	}
-	
+
 	public void empilhar(T item) {
-		
-		Celula<T> novaCelula;
-		
-		novaCelula = new Celula<T>(item);
+
+		Celula<T> novaCelula = new Celula<T>(item);
 		novaCelula.setProximo(topo);
 		topo = novaCelula;
+
 	}
-	
+
 	public T desempilhar() throws Exception {
-		
-		T desempilhado;
-		
-		if (! pilhaVazia()) {
-			desempilhado = topo.getItem();
-			topo = topo.getProximo();
-			return desempilhado;
-		} else
-			throw new Exception("Nao foi possível desempilhar "
+
+		if (pilhaVazia()) {
+			throw new NoSuchElementException("Nao foi possível desempilhar "
 					+ "o item: a pilha está vazia!");
+		}
+
+		T desempilhado = topo.getItem();
+		topo = topo.getProximo();
+		return desempilhado;
+
 	}
-	
+
 	public T consultarTopo() throws Exception {
-		
-		if (! pilhaVazia())
-			return(topo.getItem());
-		else
-			throw new Exception("A pilha está vazia!");
+
+		if (pilhaVazia()) {
+			throw new NoSuchElementException("Nao há nenhum item na pilha!");
+
+		}
+
+		return topo.getItem();
+
 	}
 }
