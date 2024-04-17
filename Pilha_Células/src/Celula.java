af4ee77e723dc9e39cdf29974b7914b17fcfc62d
Luca Azalim <lucaazalim@gmail.com>
2024-04-17 20:38:08 -0300

    Utilizando setters no construtor de Célula e encadeando construtores.

--- Pilha_Células/src/Celula.java
+++ Pilha_Células/src/Celula.java
@@ -2,28 +2,28 @@ public class Celula<T> {
 
 	private T item;
 	private Celula<T> proximo;
-	
+
 	public Celula() {
-		item = null;
-		proximo = null;
+		this(null);
 	}
-	
+
 	public Celula(T item) {
-		this.item = item;
-		proximo = null;
+		setItem(item);
+		setProximo(null);
 	}
-	
+
 	public T getItem() {
 		return item;
 	}
-	
+
 	public void setItem(T item) {
 		this.item = item;
 	}
-	
+
 	public Celula<T> getProximo() {
 		return proximo;
 	}
+
 	public void setProximo(Celula<T> proximo) {
 		this.proximo = proximo;
 	}
