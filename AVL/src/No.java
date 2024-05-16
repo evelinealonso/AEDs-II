23d4b2dd159572575e8b32a8d34dab03993dc76a
evelinealonso <69216337+evelinealonso@users.noreply.github.com>
2024-05-16 20:49:09 -0300

    Implementação da árvore AVL

---
+++ AVL/src/No.java
@@ -0,0 +1,93 @@
+public class No<T> {
+
+	private T item;        // contém os dados do item armazenado no nodo da árvore.
+	private No<T> direita;    // referência ao nodo armazenado, na árvore, à direita do nó em questão.
+	private No<T> esquerda;   // referência ao nodo armazenado, na árvore, à esquerda do nó em questão.
+	private int altura;
+	
+	public No() {
+		this.setItem(null);
+	    this.setDireita(null);
+	    this.setEsquerda(null);
+	    this.altura = 0;
+	}
+	
+	public No(T item) {
+		this.setItem(item);
+	    this.setDireita(null);
+	    this.setEsquerda(null);
+	    this.altura = 0;
+	}
+
+	public No(T item, No<T> esquerda, No<T> direita) {
+		this.setItem(item);
+	    this.setDireita(direita);
+	    this.setEsquerda(esquerda);
+	    this.altura = 0;
+	}
+
+	public T getItem() {
+		return this.item;
+	}
+
+	public void setItem(T item) {
+		this.item = item;
+	}
+
+	public No<T> getDireita() {
+		return this.direita;
+	}
+
+	public void setDireita(No<T> direita) {
+		this.direita = direita;
+	}
+
+	public No<T> getEsquerda() {
+		return this.esquerda;
+	}
+
+	public void setEsquerda(No<T> esquerda) {
+		this.esquerda = esquerda;
+	}
+	
+	public int getAltura() {
+		return this.altura;
+	}
+
+	public void setAltura() {
+		
+		int alturaEsquerda, alturaDireita;
+		
+		if (this.esquerda != null)
+			alturaEsquerda = this.esquerda.getAltura();
+		else
+			alturaEsquerda = -1;
+		
+		if (this.direita != null)
+			alturaDireita = this.direita.getAltura();
+		else
+			alturaDireita = -1;
+		
+		if (alturaEsquerda > alturaDireita)
+			this.altura = alturaEsquerda + 1;
+		else
+			this.altura = alturaDireita + 1;
+	}
+	
+	public int getFatorBalanceamento() {
+		
+		int alturaEsquerda, alturaDireita;
+		
+		if (this.esquerda != null)
+			alturaEsquerda = this.esquerda.getAltura();
+		else
+			alturaEsquerda = -1;
+		
+		if (this.direita != null)
+			alturaDireita = this.direita.getAltura();
+		else
+			alturaDireita = -1;
+		
+		return (alturaEsquerda - alturaDireita);
+	}
+}
